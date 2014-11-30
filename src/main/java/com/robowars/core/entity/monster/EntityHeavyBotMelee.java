5e87f5122fcdc1e695c2bea9e460663680f0dda5
thomas15v <tvanmellaerts@live.be>
2014-11-30 13:07:33 +0100

    Fixed health issues with the heavy bot!

--- src/main/java/com/robowars/core/entity/monster/EntityHeavyBotMelee.java
+++ src/main/java/com/robowars/core/entity/monster/EntityHeavyBotMelee.java
@@ -10,13 +10,12 @@ public class EntityHeavyBotMelee extends EntityBotMelee {
 
     public EntityHeavyBotMelee(World world) {
         super(world);
-        setHealth(80F);
     }
 
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
         getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(15.0F);
-        getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(getHealth());
+        getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(80F);
     }
 }
